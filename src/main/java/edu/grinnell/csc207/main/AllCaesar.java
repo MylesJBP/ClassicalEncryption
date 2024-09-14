a11228acf5799b0ebfc4f34ed48bda12282299f2
MylesBP <bohrerpu@grinnell.edu>
2024-09-14 17:14:08 -0500

    Fixed style

--- src/main/java/edu/grinnell/csc207/main/AllCaesar.java
+++ src/main/java/edu/grinnell/csc207/main/AllCaesar.java
@@ -22,54 +22,48 @@ public class AllCaesar {
    *   fraction or failing to set up a printwriter.
    */
 
-   public static String getED{
-
-   }
+   public static int checkMess(String message){
+    char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char[] chars = message.toCharArray();
+    for(char letter : chars){
+      int check = 0;
+      for(char valid : alphabet) {
+        if(letter == valid){
+          check = 1;
+          break;
+        } // if
+      } // for
+      if(check == 0){
+        System.err.println("Error: String contains characters other than lowercase letters.");
+        return -1;
+      } // if
+    }
+    return 0; // for
+  } // checkMess
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    int count = 0, validity = 1;
-    char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     if(args.length != 2){
       System.err.println("Error: Incorrect Number of Parameters.");
-    } // if
-    else{
-      for(String arg : args){
-        if(count == 0){
-          if(!arg.equals("decode") && !arg.equals("encode")) {
-            System.err.println("Error: Invalid option: \"" + arg + "\". Valid options are \"encode\" or \"decode\".\n");
-            validity = 0;
-            break;
-          } // if
+    } else if(!args[0].equals("decode") && !args[0].equals("encode")){
+      System.err.println("Invalid option: "+ args[0] + ". Valid options are \"encode\" or \"decode\".");
+  } else{
+      int messVal = checkMess(args[1]);
+      if(messVal != -1){
+        if(args[0].equals("encode")){
+          for(char ch = 'a'; ch <= 'z'; ch++) {
+            pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(args[1], ch));
+          } // for
         } // if
-        else if(count == 1) {
-          char[] chars = arg.toCharArray();
-          for(char letter : chars){
-            int check = 0;
-            for(char valid : alphabet){
-              if(letter == valid){
-                check = 1;
-                break;
-              } // if
-            } // for
-            if(check == 0){
-              System.err.println("Error: String contains characters other than lowercase letters.");
-              validity = 0;
-            } // if
-          } // for       
+        else if(args[0].equals("decode")){
+          for(char ch = 'a'; ch <= 'z'; ch++) {
+            pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(args[1], ch));
+          } // for
         } // else if
-        count += 1;
-      } // for
-      if(validity == 1 && args[0].equals("encode")){
-        for(char ch = 'a'; ch <= 'z'; ch++) {
-          pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(args[1], ch));
-        } // for
       } // if
-      else if(validity == 1 && args[0].equals("decode")){
-        for (char ch = 'a'; ch <= 'z'; ch++) {
-          pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(args[1], ch));
-        } // for
-      } // else if
-      pen.close();
-    } // else
+      else{
+        System.err.println("Error: Incorrect Message Values");
+      } // else
+    }// else
+    pen.close();
   } // main
 } // AllCaesar
